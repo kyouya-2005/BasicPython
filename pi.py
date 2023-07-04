efdceddeb87c64c7e8770d3cfb79bec6db0f17ab
konana-2005 <konana0202@icloud.com>
2023-07-04 22:31:05 +0900

    課題

--- pi.py
+++ pi.py
@@ -4,6 +4,7 @@ text = """
 """
 
 # TODO
-text = list(map(len, ["How", "I", "want", "a", "drink", "alcoholic", "of", "course", "after", "the", "heavy", "chapters", "involving", "quantum", "mechanics", "All", "of", "thy", "geometry", "Herr", "Planck", "is", "fairly", "hard"]))
+import re
 
-print(text)
+a = re.sub('[,.]',' ', text)
+print( *list(map(len, (a.split()))),sep='')
