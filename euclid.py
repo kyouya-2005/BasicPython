77cae12699de1e784aaf9bb15e97a252b6298ec4
konana-2005 <konana0202@icloud.com>
2023-07-15 00:02:39 +0900

    制御構文課題（再）

--- euclid.py
+++ euclid.py
@@ -1,4 +1,23 @@
-a = input("a の値を入力: ")
-b = input("b の値を入力: ")
+
+
 
 # TODO
+def gcd( a, b):
+
+    while b != 0:
+        a, b = b, a % b
+        return b
+num1 = 10
+num2 = 20
+GCD = gcd(num1, num2)
+print(f"({num1}, {num2}) -> gcd: {GCD}")
+
+num1 = 14
+num2 = 91
+GCD = gcd(num1, num2)
+print(f"({num1}, {num2}) -> gcd: {GCD}")
+
+num1 = 91
+num2 = 14
+GCD = gcd(num1, num2)
+print(f"({num1}, {num2}) -> gcd: {GCD}")
--- trapezoidal_integral.py
+++ trapezoidal_integral.py
@@ -3,3 +3,12 @@ from math import sin
 # print(sin(0))
 # >>> 0
 # -----------
+import math
+def f(x):
+    return sin(x)
+N = 100
+a = 0
+b = math.pi / 2
+h = ( b - a ) / N
+s = ( h / 2 ) * ( f(a) + 2*sum(f ( h*i ) for i in range ( 1, N - 1)) + f(b))
+print(s)
