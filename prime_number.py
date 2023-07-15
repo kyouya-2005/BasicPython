044a8ccc2b941810a5a10c39948658ba1ab0d113
konana-2005 <konana0202@icloud.com>
2023-07-16 05:56:43 +0900

    関数

--- prime_number.py
+++ prime_number.py
@@ -1,4 +1,11 @@
-a = input("aの値を入力: ")
-b = input("bの値を入力: ")
 
 # TODO
+def sosuu(n):
+    if n <= 1:
+        return False
+    for i in range( 2, int( n**0.5 ) + 1 ):
+        if n % i == 0:
+            return False
+        return True
+    
+print( sosuu( n = input( "値を入力: " ) ) )
